dacf15479c5ed5ead56676fb201530f93b48f56e
Alan Robledo <lanrobledo20@yahoo.com>
2019-04-27 23:43:05 -0700

    added description

--- quadratic.py
+++ quadratic.py
@@ -1,3 +1,25 @@
+#===============================================================================
+# Finds the roots of ax**2 + bx + c == 0 for any arbitrary coefficients that are
+# specified by the user.
+# First, a function is defined to solve the quadratic equation.
+# Then the user is asked what they want the coefficients to be.
+#===============================================================================
+# Author: Alan Robledo
+#===============================================================================
+# To test for 2 real roots use:    a = 1, b = 6, c = 3
+#             Output should be:    x1 = (-0.550510257217+0j)
+#                                  x2 = (-5.44948974278+0j)
+# To test for 1 real root use:     a = -4, b = 12, c = -9
+#             Output should be:    x1 = (1.5+0j)
+#                                  x2 = (1.5+0j)
+# To test for 2 complex roots use: a = 1, b = 2, c = 3
+#             Output should be:    x1 = (-1+1.41421356237j)
+#                                  x2 = (-1-1.41421356237j)
+#===============================================================================
+# Notes:
+# In order for numpy to compute the square root of a negative number, the
+# argument must be in the form of a complex number where j = sqrt(-1).
+#===============================================================================
 def solve_quad(a, b, c):
     solns = []
     discrim = b**2 - 4*a*c
