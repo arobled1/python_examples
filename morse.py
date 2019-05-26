e792200c32b003de240ffb5bfa6e2d68c7994eb2
Alan Robledo <lanrobledo20@yahoo.com>
2019-05-25 23:24:09 -0700

    created function for grid

--- morse.py
+++ morse.py
@@ -10,10 +10,12 @@
 # ngrid = number of grid points
 # dx = dist. between points (Remember: for every ngrid points, there
 #       are ngrid - 1 dx)
-#
 #===============================================================================
+def generate_grid(xbound, ngrid):
+    dx = (2.0 * xbound) / (ngrid - 1)
+    x_grid = [-xbound + i * dx for i in xrange(ngrid)]
+    return dx, x_grid
 
 xbound = 50
 ngrid = 2000
-dx = (2.0 * xbound) / (ngrid - 1)
-x_grid = [-xbound + i * dx for i in xrange(ngrid)]
+dx, x_grid = generate_grid(xbound, ngrid)
