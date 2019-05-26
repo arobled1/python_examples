5abec9e70338cce9950f7c471a2b745ed6bfcd6e
Alan Robledo <lanrobledo20@yahoo.com>
2019-05-25 23:13:49 -0700

    successfully computed grid

--- morse.py
+++ morse.py
@@ -1,15 +1,19 @@
 #=============================================================================80
 # Description:
-# Computing the hamiltonian for a morse oscillator on a 1D grid. 
+# Computing the hamiltonian for a morse oscillator on a 1D grid.
 #===============================================================================
 # Author: Alan Robledo
 # Date modified: May 25, 2019
 #===============================================================================
 # Variables:
-# 'xbound' = 
+# xbound = sets the bound in the positive and negative direction
+# ngrid = number of grid points
+# dx = dist. between points (Remember: for every ngrid points, there
+#       are ngrid - 1 dx)
+#
 #===============================================================================
+
 xbound = 50
 ngrid = 2000
 dx = (2.0 * xbound) / (ngrid - 1)
-x0 = -(ngrid + 1) * dx * 0.5
-x_grid = [-xbound + i * dx for i in xrange(1, ngrid)]
+x_grid = [-xbound + i * dx for i in xrange(ngrid)]
