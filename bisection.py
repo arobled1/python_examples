393a77acd98fad92b659f36f397b6bb6bd3d3e3b
Alan Robledo <ar6138@nyu.edu>
2019-09-06 16:10:47 -0400

    converted for python3

--- bisection.py
+++ bisection.py
@@ -1,13 +1,13 @@
 #===============================================================================
 # Description:
-# Finds the root of an example function between a specified interval using the 
+# Finds the root of an example function between a specified interval using the
 # bisection method.
 #===============================================================================
 # Author: Alan Robledo
 #===============================================================================
-# Output: 
+# Output:
 # The calculated root is:  0.641184806824
-# Number of iterations:  21 
+# Number of iterations:  21
 #===============================================================================
 import math
 
@@ -25,27 +25,27 @@ def bisection(a, b, tol):
         fb = func(b)
         fc = func(c)
         if fa == 0:
-            print "Your value for left interval turned out to be the root of the function"
+            print("Your value for left interval turned out to be the root of the function")
         elif fb == 0:
-            print "Your value for right interval turned out to be the root of the function"
+            print("Your value for right interval turned out to be the root of the function")
         elif fc == 0 or (b-a)/2.0 < tol:
             i = max_iterations + 1
-	elif counter == max_iterations:
-	    print "Uh oh, you've reached the maximum number of iterations."
-	    i = max_iterations + 1
+        if counter == max_iterations:
+	        print("Uh oh, you've reached the maximum number of iterations.")
+	        i = max_iterations + 1
         if fa*fc > 0:
             a = c
         else:
             b = c
     return c, counter
 
-print """We're gonna find the root for
+print("""We're gonna find the root for
 f(x) = x - 2^-x
-on [0,2] with a tolerance of 10^-6."""
+on [0,2] with a tolerance of 10^-6.""")
 a = 0.0
 b = 2.0
-print "\nUsing the bisection method,"
+print("\nUsing the bisection method,")
 tol = 10**-6
 root, counter = bisection(a, b, tol)
-print "The calculated root is: ", root
-print "# of iterations: ", counter
+print("The calculated root is: ", root)
+print("# of iterations: ", counter)
