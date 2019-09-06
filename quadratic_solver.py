69ea98f248704c1bc421e640ad3ef49b46faa8ab
Alan Robledo <ar6138@nyu.edu>
2019-09-06 16:42:23 -0400

    converted for python3

--- quadratic_solver.py
+++ quadratic_solver.py
@@ -20,6 +20,8 @@
 # In order for numpy to compute the square root of a negative number, the
 # argument must be in the form of a complex number where j = sqrt(-1).
 #===============================================================================
+import numpy as np
+
 def solve_quad(a, b, c):
     solns = []
     discrim = b**2 - 4*a*c
@@ -27,13 +29,13 @@ def solve_quad(a, b, c):
     solns.append( (-b - np.sqrt(discrim + 0j)) / (2*a) )
     return solns
 
-a = float(raw_input("What is a? "))
-b = float(raw_input("What is b? "))
-c = float(raw_input("What is c? "))
+a = float(input("What is a? "))
+b = float(input("What is b? "))
+c = float(input("What is c? "))
 solutions = solve_quad(a, b, c)
 if solutions[0] == solutions[1]:
-    print "Uh oh, there's only one root."
-    print 'x1 = {0}\nx2 = {1}'.format(*solutions)
+    print("Uh oh, there's only one root.")
+    print('x1 = {0}\nx2 = {1}'.format(*solutions))
 else:
-    print "Here are your solutions:"
-    print 'x1 = {0}\nx2 = {1}'.format(*solutions)
+    print("Here are your solutions:")
+    print('x1 = {0}\nx2 = {1}'.format(*solutions))
