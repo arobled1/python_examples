06ab04f9c4c4d374f1f20becf337ead1c990ad26
Alan Robledo <ar6138@nyu.edu>
2019-09-06 15:17:22 -0400

    converted for python3

--- binary.py
+++ binary.py
@@ -4,12 +4,12 @@
 # decimal integer to base-2 binary.
 # If "b2d" is entered, the binary is split into a list with each 1 or 0 being
 # considered an element of the list.
-# Since 'raw_input' reads input as a string, each element is converted from a
+# Since 'input()' reads input as a string, each element is converted from a
 # string to an integer.
 # Then each element from right to left is multiplied by its binary value
 # and each product is summed until the result is the value of the entire
 # binary in decimal form.
-# If "d2b" is entered, the 'raw_input' is converted from a string to an integer.
+# If "d2b" is entered, the 'input()' is converted from a string to an integer.
 # For any integer a, a mod 2 will always result in a 1 or a 0.
 # Therefore, the code iteratively computes floor divisions of the decimal by
 # 2**j mod 2 and appends the resulting 0 or 1 to a list.
@@ -35,7 +35,7 @@
 # them into integers, which it shouldn't!
 #===============================================================================
 def binary_to_decimal():
-    usr_bin = raw_input("Type your binary: ")
+    usr_bin = input("Type your binary: ")
     bin_list = list(usr_bin)
     bin_list = [ int(x) for x in bin_list ]
     decimal = 0
@@ -51,15 +51,17 @@ def binary_to_decimal():
     return decimal
 
 def decimal_to_binary():
-    usr_dec = raw_input("Type your decimal: ")
+    usr_dec = input("Type your decimal: ")
     dec = int(usr_dec)
+    print(dec)
     binary = []
     i = 0
     j = 0
     sum = 0
     while i == 0:
-        binary.append((dec/(2**j) % 2))
-        if (dec/(2**j) % 2) == 1:
+        binary.append((dec//(2**j) % 2))
+        print(dec//(2**j) % 2)
+        if (dec//(2**j) % 2) == 1:
             sum = sum + 2**j
         j = j + 1
         if sum == dec:
@@ -69,13 +71,13 @@ def decimal_to_binary():
     print_bin = ''.join(binary)
     return print_bin
 
-print """Do you want to convert binary to decimal (enter 'b2d')?
+print ("""Do you want to convert binary to decimal (enter 'b2d')?
 or
-Do you want to convert decimal to binary (enter 'd2b')?"""
-choice = raw_input()
+Do you want to convert decimal to binary (enter 'd2b')?""")
+choice = input()
 if choice == "b2d":
     decimal = binary_to_decimal()
-    print "Decimal is: %d" % decimal
+    print ("Decimal is: %d" % decimal)
 elif choice == "d2b":
     print_bin = decimal_to_binary()
-    print "Binary is: %s" % print_bin
+    print ("Binary is: %s" % print_bin)
