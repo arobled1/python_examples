f8db45a5c55fe405e1f5b0bbadc4fefe7d999c00
Alan Robledo <ar6138@nyu.edu>
2019-09-06 16:33:41 -0400

    converted for python3

--- newton.py
+++ newton.py
@@ -18,29 +18,29 @@ def func_prime(xin):
     return 1 + math.log(2)*2**(-xin)
 
 def newton(a, tol):
-        i = 0
-        counter = 0
-	max_iterations = 100
-        while i <= max_iterations:
-            counter = counter + 1
-            fa = func(a)
-            fa_prime = func_prime(a)
-            a_new = a - fa / fa_prime
-            if abs(a_new - a) < tol:
-                i = max_iterations + 1
-	    elif counter == max_iterations:
-		print "Uh oh, you've reached the maximum number of iterations."
-                i = max_iterations + 1
-            else:
-                a = a_new
-        return a, counter
+    i = 0
+    counter = 0
+    max_iterations = 100
+    while i <= max_iterations:
+        counter = counter + 1
+        fa = func(a)
+        fa_prime = func_prime(a)
+        a_new = a - fa / fa_prime
+        if abs(a_new - a) < tol:
+            i = max_iterations + 1
+        elif counter == max_iterations:
+            print("Uh oh, you've reached the maximum number of iterations.")
+            i = max_iterations + 1
+        else:
+            a = a_new
+    return a, counter
 
-print """We're gonna find the root for
+print("""We're gonna find the root for
 f(x) = x - 2^-x
-with an initial solution of p_o = 2 and a tolerance of 10**-5."""
-print "\nUsing newton's method,"
+with an initial solution of p_o = 2 and a tolerance of 10**-5.""")
+print("\nUsing newton's method,")
 a = 2
 tol = 10**-5
 root, counter = newton(a, tol)
-print "The calculated root is: ", root
-print "# of iterations: ", counter
+print("The calculated root is: ", root)
+print("# of iterations: ", counter)
