ca66b45a5c1fad4fb333fb7c6b98716a258719d1
Alan Robledo <ar6138@nyu.edu>
2019-09-06 17:11:00 -0400

    converted for python3

--- rand_plot.py
+++ rand_plot.py
@@ -1,4 +1,4 @@
-#===============================================================================
+ #===============================================================================
 # Description:
 # Generates a list of 25 numbers and plots each number.
 # Coordinates for plotting are made by making each random number the y
@@ -6,14 +6,16 @@
 #===============================================================================
 # Author: Alan Robledo
 #===============================================================================
+import random
+import matplotlib.pyplot as plt
 x = range(1,26)
 y = []
 for i in x:
     y.append(random.randint(65,75))
-print "Below are the coordinates. Your random numbers are the y-coordinates."
-print "x , y"
+print("Below are the coordinates. Your random numbers are the y-coordinates.")
+print("x , y")
 for i in x:
-    print x[i-1], ",", y[i-1]
+    print(x[i-1], ",", y[i-1])
 plt.xlim(0,26)
 plt.ylim(60,80)
 plt.plot(x, y, 'o', color='blue')
